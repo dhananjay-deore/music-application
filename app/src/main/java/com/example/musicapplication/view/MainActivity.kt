bb09ba1f9c86536279b82a07ccc05eaf334ac526
DDEORE <ddeore@visteon.com>
2024-06-12 17:44:31 +0530

    add splash screen

--- app/src/main/java/com/example/musicapplication/view/MainActivity.kt
+++ app/src/main/java/com/example/musicapplication/view/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.example.musicapplication
+package com.example.musicapplication.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.musicapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_MusicApplication)
+
         setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/example/musicapplication/view/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.example.musicapplication.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.musicapplication.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            this.finish()
+        },1000)
+    }
+}
