e6eee0decfd89751f83b23e6d73445bd72dbaa6c
DDEORE <ddeore@visteon.com>
2024-06-14 16:59:30 +0530

    player activity ui

--- app/src/main/java/com/example/musicapplication/view/SplashActivity.kt
+++ app/src/main/java/com/example/musicapplication/view/SplashActivity.kt
@@ -1,12 +1,11 @@
 package com.example.musicapplication.view
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
-import com.example.musicapplication.R
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     private val TAG = "SplashActivity"
