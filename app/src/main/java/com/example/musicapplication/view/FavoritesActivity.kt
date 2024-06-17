1b3ab549fc8888ea44efa68ed0387dd511bd7005
DDEORE <ddeore@visteon.com>
2024-06-17 11:23:49 +0530

    playlist & favourites activity basic design

---
+++ app/src/main/java/com/example/musicapplication/view/FavoritesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.musicapplication.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.musicapplication.R
+
+class FavoritesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorites)
+    }
+}
---
+++ app/src/main/java/com/example/musicapplication/view/Playlist_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.musicapplication.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.musicapplication.R
+
+class Playlist_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_playlist)
+    }
+}
